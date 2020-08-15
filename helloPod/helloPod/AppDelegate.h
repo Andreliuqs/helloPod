62f81f806700eaf68023229b251185ca00af3214
Liuqs <liuqingsheng@lqsdeMacBook-Air.local>
2020-08-15 10:51:29 +0800

    firstCommit

---
+++ helloPod/helloPod/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  helloPod
+//
+//  Created by Liuqs on 2020/8/15.
+//  Copyright © 2020 helloPod. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
